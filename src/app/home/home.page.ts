221a92c6168b2b1bd5dbb0c4161ab9ecfddad8b4
Alamgir Qazi <alamgirmunirqazi@gmail.com>
2019-11-29 05:41:09 +0500

    added sample code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { DatalistingService } from '../datalisting.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +9,43 @@ import { DatalistingService } from '../datalisting.service';
   styleUrls: ['home.page.scss']
 })
 export class HomePage implements OnInit {
-  constructor(private datalistingService: DatalistingService) {}
+  constructor(
+    private datalistingService: DatalistingService,
+    private httpClient: HttpClient
+  ) {}
   loading = false;
   quotes = [];
 
   ngOnInit() {
-    this.getAll();
+    // this.getAll();
+    this.getAllObservables();
   }
 
-  getAll() {
+  async getAllAwait() {
+    const result = await this.datalistingService.getQuotes().toPromise();
+    console.log('result', result);
+    this.quotes = result.data;
+    console.log('asdasdasd vasdasdasd');
+  }
+  async getAllPromise() {
+    const promise = new Promise((resolve, reject) => {
+      const url = `https://random-quotes.now.sh/get-all-quotes`;
+      resolve(this.httpClient.get(url).toPromise());
+    });
+    promise
+      .then(res => {
+        console.log('res', res);
+        this.quotes = res['data'];
+      })
+      .catch(function(ex) {
+        console.log('ex', ex);
+      });
+
+    console.log('xyz asdasd');
+    console.log('xyz asdasdv123123 ');
+  }
+
+  getAllObservables() {
     this.loading = true;
 
     console.log('get');
